9324e680c3825b49f3c811ede69b5662cec51311
Jonny Quezada <104032385+SoyDeSoya23@users.noreply.github.com>
2022-05-02 12:43:46 -0500

    Add files via upload
    Added original classes.py and main.py from Lab 09; created test_classes.py

---
+++ classes.py
@@ -0,0 +1,44 @@
+class Television:
+    MIN_CHANNEL = 0     # Minimum TV channel
+    MAX_CHANNEL = 3     # Maximum TV channel
+
+    MIN_VOLUME = 0      # Minimum TV volume
+    MAX_VOLUME = 2      # Maximum TV volume
+
+    def __init__(self):
+        self.__channel = Television.MIN_CHANNEL
+        self.__volume = Television.MIN_VOLUME
+        self.__status = False
+
+    def power(self):
+        if self.__status is False:
+            self.__status = True
+        else:
+            self.__status = False
+
+    def channel_up(self):
+        if self.__status is True:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.__status is True:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
+    def volume_up(self):
+        if self.__status is True:
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self):
+        if self.__status is True:
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+
+    def __str__(self):
+        return f'TV status: is on = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
